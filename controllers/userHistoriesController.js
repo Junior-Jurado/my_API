ef978e21671cef02079295dca7fecb171fe1790d
Junior-Jurado <juniorjurado2004@gmail.com>
2024-02-29 20:41:42 -0500

    Subiendo a producción 3

--- controllers/userHistoriesController.js
+++ controllers/userHistoriesController.js
@@ -1,6 +1,7 @@
 const UserHistory = require('../models/user_history');
 const Project = require('../models/project');
 const Task = require('../models/task');
+const User = require('../models/user')
 
 
 module.exports = {
@@ -17,22 +18,32 @@ module.exports = {
             existProject = await Project.search_project(data.project_id);
 
             if (existProject != null) {
-                id_history = await UserHistory.create(data, existProject);
-                const myData = {
-                    user: data.user,
-                    id: id_history,
-                    desciption: data.desciption,
-                    criteria: data.criteria,
-                    state: data.state_id,
-                    created_by: existProject.created_by_id,
-                    project: existProject.id 
-                };
 
-                return res.status(201).json({
-                    success: true,
-                    message: 'El registro se realizó correctamente!!',
-                    data: myData
-                });
+                const user = await User.findByIdPerRol(data.created_by_id);
+
+                if (user['rol'] == 1) {
+                    id_history = await UserHistory.create(data, existProject);
+                    const myData = {
+                        user: data.user,
+                        id: id_history,
+                        desciption: data.desciption,
+                        criteria: data.criteria,
+                        state: data.state_id,
+                        created_by: existProject.created_by_id,
+                        project: existProject.id 
+                    };
+
+                    return res.status(201).json({
+                        success: true,
+                        message: 'El registro se realizó correctamente!!',
+                        data: myData
+                    });
+                } else {
+                    return res.status(401).json({
+                        success: false,
+                        message: 'No se puede crear la historia de Usuario, ya que eres un gerente!'
+                    }); 
+                }
             } else {
                 return res.status(401).json({
                     success: false,
--- routes/userHistoryRoutes.js
+++ routes/userHistoryRoutes.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
    
     app.post('/api/userHistory/create', passport.authenticate('jwt', {session: false}), UserHistoriesController.create);
     app.post('/api/userHistory/update', passport.authenticate('jwt', {session: false}), UserHistoriesController.update);
+    
     app.delete('/api/userHistory/delete', passport.authenticate('jwt', {session: false}), UserHistoriesController.delete);
     
 }
