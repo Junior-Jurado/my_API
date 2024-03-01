80e297b0c258b929527da4124b1848b6f08870b0
Junior-Jurado <juniorjurado2004@gmail.com>
2024-02-29 19:59:46 -0500

    Subiendo a producción

--- config/config.js
+++ config/config.js
@@ -3,6 +3,12 @@ const options = {
     promiseLib: promise,
     query: (e) =>{}
 }
+const https = require('https');
+
+// Opción para deshabilitar la validación del certificado
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 const pgp = require('pg-promise')(options);
 const types = pgp.pg.types;
@@ -16,7 +22,7 @@ const databaseConfig = {
     'database':'d3govvltg5de2a',
     'user': 'mkqffxyekrbckg',
     'password': '12cd12c4738f6bd9d8b2a20c8d29470b88db4068e38fb0439d3dc6d03e10d992',
-    'ssl': { rejectUnathorized: false }
+    'ssl': { rejectUnauthorized: false }
 };
 
 const db = pgp(databaseConfig);
